e526c67609fe8b849437a4f7aee3712407b9064c
MRimpoWHITE <phichitphu2015@gmail.com>
2023-08-13 02:02:30 +0700

    GaiTod

--- Homework/HW05/hw05-8-max-value.cpp
+++ Homework/HW05/hw05-8-max-value.cpp
@@ -20,3 +20,53 @@
         The 1rd Number is the greatest among three
 
 */
+
+#include<stdio.h>
+
+int main(){
+
+    int ip1 = 0 ,ip2 = 0 ,ip3 = 0 ;
+
+    printf("Input[1-2-3] :\n");
+    scanf("%d %d %d" , &ip1 , &ip2, &ip3);
+
+    printf("Output :\n");
+
+    printf("1st Number = %d\n2nd Number = %d\n3rd Number = %d\n" , ip1 , ip2 , ip3);
+
+    if (ip1 > ip2 && ip1 > ip3)
+    {
+        printf("The 1rd Number is the greatest among three");
+    }
+    else if (ip2 > ip1 && ip2 > ip3)
+    {
+        printf("The 2nd Number is the greatest among three");
+    }else if (ip3 > ip2 && ip3 > ip1)
+    {
+        printf("The 3rd Number is the greatest among three");
+    }
+    
+
+    return 0 ;
+}
+
+
+
+// for (int i; i <= 3; i++)
+//     {
+
+//         if (Ip1 > Ip2 || Ip1 > Ip3)
+//         {
+//             printf("%d %d %d", Ip1, Ip2, Ip3);
+//         }
+
+//         else if (Ip2 > Ip1 || Ip2 > Ip3)
+//         {
+//             printf("%d %d %d", Ip2, Ip1, Ip3);
+//         }
+
+//         else if (Ip3 > Ip1 || Ip3 > Ip2)
+//         {
+//             printf("%d %d %d", Ip3, Ip2, Ip1);
+//         }
+//     }
